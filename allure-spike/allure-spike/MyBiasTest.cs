81edcdaf396b4128ef3a53b5389c509ada7a148f
Andrey Volkov <avolkov.dev.10x@gmail.com>
2021-09-11 20:19:10 +0300

    Add sample unit test prj

---
+++ allure-spike/allure-spike/MyBiasTest.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+
+namespace allure_spike
+{
+    public class MyBiasTest
+    {
+        [Fact]
+        public void Run()
+        {
+            Assert.True("I like K-pop music".GetHashCode() % 2 == 0);
+        }
+        
+    }
+}
