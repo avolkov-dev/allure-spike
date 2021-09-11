be2edbe9e8f6a4ebc7ffb9eb41333cedb5a7d7f4
Andrey Volkov <avolkov.dev.10x@gmail.com>
2021-09-11 20:22:20 +0300

    Setup Allure

---
+++ README.md
@@ -0,0 +1,2 @@
+# .NET/Allure/Github pages integration example
+ https://avolkov-dev.github.io/allure-spike/
---
+++ src/MyBiasTest.cs
@@ -0,0 +1,26 @@
+﻿using Dapper;
+using Npgsql;
+using Xunit;
+
+namespace allure_spike
+{
+    public class MyBiasTest
+    {
+        [Fact]
+        public void Run()
+        {
+            Assert.True("I like K-pop music".GetHashCode() % 2 == 0);
+        }
+        
+        
+        
+
+        [Fact]
+        public void ExecuteSql()
+        {
+            using var con = new NpgsqlConnection("Server=localhost;Port=26257;Username=root;Database=postgres");
+            con.Query("select 1;");
+        }
+        
+    }
+}
